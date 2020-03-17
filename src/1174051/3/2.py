a3960434748fdbf41787341c35c92b51c35a47f2
eviesujadi <eviesujadi16@gmail.com>
2020-03-17 15:44:50 +0700

    Evietania 1174051 Chap 3

---
+++ src/1174051/3/2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+epi = np.arange(6)
+print(epi)
+
+#array 3dimensi
+epik = np.arange(36).reshape(4,3,3)
+print(epik)
+
+#penjumlahan array
+nilai_array_1 = np.array([1,2,3,4])
+nilai_array_2 = np.array([20,30,40,50])
+nilai_array_3 = np.arange(4)
+Hasil = nilai_array_1+nilai_array_3+nilai_array_3
+print(nilai_array_1)
+print(nilai_array_3)
+print(Hasil)
