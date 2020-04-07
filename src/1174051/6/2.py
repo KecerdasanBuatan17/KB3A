e68c455fd9aa774e82aaa71507e04bf8f02cebfa
eviesujadi <eviesujadi16@gmail.com>
2020-04-07 14:13:06 +0700

    hahhh

---
+++ src/1174051/6/2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 13:29:58 2020
+
+@author: Sujadi
+"""
+
+def display_mfcc(song):
+    y, _ = librosa.load(song)
+    mfcc = librosa.feature.mfcc(y)
+
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(mfcc, x_axis='time', y_axis='mel')
+    plt.colorbar()
+    plt.title(song)
+    plt.tight_layout()
+    plt.show()
---
+++ src/1174051/6/9.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 13:33:41 2020
+
+@author: Sujadi
+"""
+
+model.fit(train_input, train_labels, epochs=10, batch_size=32,
+          validation_split=0.2)
